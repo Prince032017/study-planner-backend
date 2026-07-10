902b98c899aca726d6901d8434c83666c5317195
Prince032017 <pt13081999@gmail.com>
2026-07-10 16:54:55 +0530

    make code more flexible by introducing global variable

--- src/main/java/com/Jee/study_planner/config/CorsConfig.java
+++ src/main/java/com/Jee/study_planner/config/CorsConfig.java
@@ -1,18 +1,19 @@
 package com.Jee.study_planner.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${app.cors.allowed-origins}")
+    private String [] allowedOrigins;
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
-                .allowedOrigins(
-                        "http://localhost:5173",    // React dev server
-                        "http://localhost:3000"   // CRA dev server
-                ).allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);
     }
